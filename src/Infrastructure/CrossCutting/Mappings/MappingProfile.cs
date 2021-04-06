60141ae68b0dd60f4f13d72a9a9b537a99e323f5
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2021-04-05 22:46:22 -0300

    fix AutoMapper comment

--- src/Infrastructure/CrossCutting/Mappings/MappingProfile.cs
+++ src/Infrastructure/CrossCutting/Mappings/MappingProfile.cs
@@ -10,7 +10,7 @@ namespace CrossCutting.Mappings
         {
             // User
             CreateMap<UserCreateDto, User>();
-            // Opts will prevent AutoMapper to replace fields that not will be updated to null
+            // This option will prevent AutoMapper to replace fields that not will be updated to null
             CreateMap<UserUpdateDto, User>()
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
             CreateMap<User, UserResultDto>().ReverseMap();
