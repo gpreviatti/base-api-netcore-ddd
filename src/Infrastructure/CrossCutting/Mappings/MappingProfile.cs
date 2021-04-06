d4cf87c73f9a4c51bc074dc317ebaa3d28c87614
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2021-04-05 22:38:45 -0300

    fix mapping profile ident

--- src/Infrastructure/CrossCutting/Mappings/MappingProfile.cs
+++ src/Infrastructure/CrossCutting/Mappings/MappingProfile.cs
@@ -11,7 +11,8 @@ namespace CrossCutting.Mappings
             // User
             CreateMap<UserCreateDto, User>();
             // Opts will prevent AutoMapper to replace fields that not will be updated to null
-            CreateMap<UserUpdateDto, User>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<UserUpdateDto, User>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
             CreateMap<User, UserResultDto>().ReverseMap();
         }
     }
